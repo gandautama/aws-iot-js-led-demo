84556c3a76cb97e1e7c7587950b4c5bd3c0d4c43
g.kusuma <g.kusuma@samsung.com>
2018-02-12 16:50:27 +0700

    use shadow

--- led.js
+++ led.js
@@ -2,48 +2,54 @@
 //import required modules
 
 var awsIot = require('aws-iot-device-sdk');
-var rpio = require('rpio');
+//var rpio = require('rpio');
+console.log('starting');
 
-//initialize GPIO18 to low
-rpio.open(12, rpio.OUTPUT, rpio.LOW);
+//initialize GPIO21 to low
+//rpio.open(40, rpio.OUTPUT, rpio.LOW);
+console.log('GPIO21');
+
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var LED = new Gpio(21, 'out'); //use GPIO pin 4 as output
 
 //setup paths to certificates
 var device = awsIot.device({
-   keyPath: '../private.pem.key',
-  certPath: '../certificate.pem.crt',
-    caPath: '../caCert.crt',
-  clientId: 'MyThingName',
-    region: 'us-east-2'
+   keyPath: './cert/myraspi.private.key',
+  certPath: './cert/myraspi.cert.pem',
+    caPath: './cert/root-CA.crt',
+  clientId: 'myraspi',
+    region: 'us-west-2',
+	host: 'a3gtmzhen03hjs.iot.us-west-2.amazonaws.com'
 });
 
 device
   .on('connect', function() {
-
+	console.log('on connect...');
     //subscribe to the LED topic
-    device.subscribe('LED');
-
+    device.subscribe('$aws/things/myraspi/shadow/update/delta');
+    //device.subscribe('LED');
     });
+//$aws/things/myraspi/shadow/update/delta
+//{"state":{
+//"light":"on"
+//}}
 
 device
   .on('message', function(topic, payload) {
 
     // convert the payload to a JSON object
     var payload = JSON.parse(payload.toString());
-
-    console.log(payload.light);
+    console.log(payload);
 
     //check for TOPIC name
-    if(topic == 'LED'){
-
-        if(payload.light == 'on'){
-
-          rpio.write(12, rpio.HIGH);
-
+    if(topic == '$aws/things/myraspi/shadow/update/delta' || topic == 'LED'){
+        if(payload.state.light == 'on'){
+         //rpio.write(40, rpio.HIGH);
+			LED.writeSync(1); //turn LED on or off depending on the button state (0 or 1)
         } else {
-
-          rpio.write(12, rpio.LOW);
-
+        	 LED.writeSync(0);
         }
     }
+
 
   });
